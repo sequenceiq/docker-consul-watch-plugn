439f8c6556c29a4e0ff98750e9eff3e14fb1d0e0
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-07-13 16:11:06 +0200

    CLOUD-958 remove fix nameserver

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-: ${BRIDGE_IP:="172.17.42.1"}
+: ${CONSUL_HOST:="172.17.42.1"}
 : ${CONSUL_HTTP_PORT:="8500"}
 : ${CONSUL_RPC_PORT:="8400"}
 
@@ -10,20 +10,9 @@ debug() {
   [[ "DEBUG" ]]  && echo "[DEBUG] $@" 1>&2
 }
 
-# --dns isn't available for: docker run --net=host
-# sed -i /etc/resolf.conf fails:
-# sed: cannot rename /etc/sedU9oCRy: Device or resource busy
-# here comes the tempfile workaround ...
-fix-nameserver() {
-  cat>/etc/resolv.conf<<EOF
-nameserver $BRIDGE_IP
-search service.consul node.consul
-EOF
-}
-
 wait-for-consul() {
   while : ; do
-    consul members --rpc-addr=$BRIDGE_IP:$CONSUL_RPC_PORT
+    consul members --rpc-addr=$CONSUL_HOST:$CONSUL_RPC_PORT
     [[ $? == 0 ]] && break
     [[ $? != 0 ]] && sleep 5
   done
@@ -32,13 +21,12 @@ wait-for-consul() {
 start-watch() {
   ln -sf /lib/libpthread-2.18.so /lib/libpthread.so.0
   wait-for-consul
-  consul watch --http-addr=$BRIDGE_IP:$CONSUL_HTTP_PORT --type=event /consul-event-handler.sh 2>> /var/log/consul-watch/consul_handler_errors.log &
+  consul watch --http-addr=$CONSUL_HOST:$CONSUL_HTTP_PORT --type=event /consul-event-handler.sh 2>> /var/log/consul-watch/consul_handler_errors.log &
   sleep 5
   ln -sf /lib/libpthread-0.9.33.2.so  /lib/libpthread.so.0
 }
 
 main() {
-  fix-nameserver
   start-watch >> /tmp/startup.log 2>&1
   echo NOTHING TO DO>> /tmp/tmp.log
   while true; do
